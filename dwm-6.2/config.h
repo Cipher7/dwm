aa495f8026f34a6f64604edb07f433d7476a8a92
Cipher007 <>
2021-10-04 08:55:34 +0530

    Added Flameshot modkey

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -72,9 +72,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "firefox", NULL };
-static const char *dmenu_pentest[]  = { "/opt/dmenu_pentest/dmenu_pentest.sh", NULL };  /* full path of dmenu_pentest */
+static const char *dmenu_pentest[]  = { "/bin/bash /opt/dmenu_pentest/dmenu_pentest.sh", NULL };  /* full path of dmenu_pentest */
 static const char *file_manager[]  = { "thunar", NULL };
-static const char *flameshot[]  = { "flameshot gui", NULL };
+static const char *flameshot[]  = { "flameshot", "gui", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -84,7 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenu_pentest } },
 	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = file_manager } },
-	{ MODKEY,    		        XK_q,      spawn,          {.v = flameshot } },
+	{ MODKEY,                       XK_q,      spawn,          {.v = flameshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
