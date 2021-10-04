e67ec5530fe618eee3eaf4608c5e92f84595f656
Cipher007 <>
2021-10-04 08:08:44 +0530

    Changed terminal to st

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "qterminal", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char *dmenu_pentest[]  = { "/opt/dmenu_pentest/dmenu_pentest.sh", NULL };  /* full path of dmenu_pentest */
 static const char *file_manager[]  = { "thunar", NULL };
